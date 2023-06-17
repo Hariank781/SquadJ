a292932e6cd784d56ed786c28c2d585405bf715b
Hariank <hariankd_cs20.rvitm@rvei.edu.in>
2023-06-17 10:09:47 +0530

    Initial commit

---
+++ app/src/main/java/com/example/squadject/OtpJoin.java
@@ -0,0 +1,14 @@
+package com.example.squadject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OtpJoin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_otp_join);
+    }
+}
---
+++ app/src/main/java/com/example/squadject/Password_Change.java
@@ -0,0 +1,14 @@
+package com.example.squadject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Password_Change extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_password_change);
+    }
+}
