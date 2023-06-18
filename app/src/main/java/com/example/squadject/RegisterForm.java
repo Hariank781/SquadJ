db71bb84786c7033a4506d88b843174f2e904a81
Hariank <hariankd_cs20.rvitm@rvei.edu.in>
2023-06-18 10:24:11 +0530

    Commit

--- app/src/main/java/com/example/squadject/RegisterForm.java
+++ app/src/main/java/com/example/squadject/RegisterForm.java
@@ -3,42 +3,12 @@ package com.example.squadject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 
 public class RegisterForm extends AppCompatActivity {
-    private EditText nameEditText;
-    private EditText emailEditText;
-    private EditText passwordEditText;
-    private EditText repasswordEditText;
-    private EditText collegeEditText;
-    private EditText semesterEditText;
-    private EditText branchEditText;
-    private Button nextButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_form);
-
-        // Initialize EditTexts
-        nameEditText = findViewById(R.id.nameText_registerForm);
-        emailEditText = findViewById(R.id.emailText_registerForm);
-        passwordEditText = findViewById(R.id.passwordText_registerForm);
-        repasswordEditText = findViewById(R.id.repasswordText_registerForm);
-        collegeEditText = findViewById(R.id.collegeText_registerForm);
-        semesterEditText = findViewById(R.id.semesterText_registerForm);
-        branchEditText = findViewById(R.id.branchText_registerForm);
-
-        // Initialize Next button
-        nextButton = findViewById(R.id.next_registerForm);
-        nextButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Perform action on button click
-                // Add your code logic here
-            }
-        });
     }
 }
